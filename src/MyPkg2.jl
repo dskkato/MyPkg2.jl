9ff1f1fce63efc04abb7f6dda04cf550492bafbc
Daisuke Kato <kato.daisuke429@gmail.com>
2018-12-29 21:11:29 +0900

    add sample codes

--- src/MyPkg2.jl
+++ src/MyPkg2.jl
@@ -1,5 +1,15 @@
 module MyPkg2
 
+include("func.jl")
+
+export great,
+       f,
+       g,
+       func
+
 greet() = print("Hello World!")
 f() = π
+
+g() = 100
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,12 @@ using Test
 
 @testset "MyPkg2.jl" begin
     # Write your own tests here.
+
+    @test π == f()
+    @test 100 == g()
+    @testset "func" begin
+        @test 3 == func(1, 2)
+        @test 4 == func(1, 3)
+    end
+
 end
